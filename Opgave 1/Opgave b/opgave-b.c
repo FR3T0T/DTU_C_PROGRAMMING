0f5db9ad66b9a23e40cd369724399b694bb7edd5
FR3T0T <fr3t0t@gmail.com>
2024-09-14 23:39:29 +0200

    sad

--- Opgave 1/Opgave b/opgave-b.c
+++ Opgave 1/Opgave b/opgave-b.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-
-bool er_heltal(const char* s) {
-    if (*s == '\0') return false;  // Tom streng
-    if (*s == '-') s++;  // Spring over minus-tegnet hvis det er der
-    while (*s) {
-        if (!isdigit(*s)) return false;
-        s++;
-    }
-    return true;
-}
-
-bool er_delelig_med_5(int n) {
-    return n % 5 == 0;
-}
 
 int main() {
-    char input[100];
-    printf("Indtast et heltal: ");
-    if (fgets(input, sizeof(input), stdin) == NULL) {
-        printf("Fejl ved læsning af input.\n");
-        return 1;
-    }
+    int a, b, sum;
 
-    // Fjern newline character fra input hvis den er der
-    size_t len = strlen(input);
-    if (len > 0 && input[len-1] == '\n') {
-        input[len-1] = '\0';
-    }
+    // Indlæs to tal fra tastaturet
+    printf("Indtast to tal (adskilt af mellemrum): ");
+    scanf("%d %d", &a, &b);
 
-    if (er_heltal(input)) {
-        int tal = atoi(input);
-        if (er_delelig_med_5(tal)) {
-            printf("%d er delelig med 5.\n", tal);
-        } else {
-            printf("%d er ikke delelig med 5.\n", tal);
-        }
-    } else {
-        printf("Fejl: Du har ikke indtastet et gyldigt heltal.\n");
+    // Beregn summen
+    sum = a + b;
+
+    // Udskriv summen
+    printf("Summen er: %d\n", sum);
+
+    // Tjek om sum er større end b
+    if (sum > b) {
+        // Beregn difference og gem i a
+        a = sum - b;
+        printf("Sum er større end b. Ny værdi af a: %d\n", a);
     }
 
     return 0;
