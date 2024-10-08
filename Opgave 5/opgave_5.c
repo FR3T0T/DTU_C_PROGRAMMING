f2e8e30fb71947db1fcbae211baa235de9515dcd
FR3T0T <fr3t0t@gmail.com>
2024-10-08 10:20:16 +0200

    done

--- Opgave 5/opgave_5.c
+++ Opgave 5/opgave_5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 int countCharactersInSet(const char* text, const char* set) {
     int count = 0;
-    
     while (*text != '\0') {
         const char* setPtr = set;
         while (*setPtr != '\0') {
@@ -14,14 +14,22 @@ int countCharactersInSet(const char* text, const char* set) {
         }
         text++;
     }
-    
     return count;
 }
 
 int main() {
-    const char* text = "Denmark is a democratic country";
+    const int maxlength = 100;  // define array size
+    char mytext[maxlength];     // array to contain text
     const char* vowels = "aeiouAEIOU";
-    int vowelCount = countCharactersInSet(text, vowels);
-    printf("Antal vokaler i \"%s\": %d\n", text, vowelCount);
+
+    printf("Indtast en tekst (max %d tegn): ", maxlength - 1);
+    fgets(mytext, maxlength, stdin);  // read text from console
+
+    // Remove newline character if present
+    mytext[strcspn(mytext, "\n")] = 0;
+
+    int vowelCount = countCharactersInSet(mytext, vowels);
+    printf("Antal vokaler i \"%s\": %d\n", mytext, vowelCount);
+
     return 0;
 }
