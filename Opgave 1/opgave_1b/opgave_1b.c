ff9bfc462a1fce4d6b97442542e126b938dec038
FR3T0T <fr3t0t@gmail.com>
2024-12-11 21:43:18 +0100

    opgave 1 med mermaid flowchart

--- Opgave 1/opgave_1b/opgave_1b.c
+++ Opgave 1/opgave_1b/opgave_1b.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
-
 int main()
 {
-    // a, b, d og sum vil blive defineret senere i koden fra input.
-    int a, b, d, sum;
-
-        // Instruks udskift & scanf til definering af a og b.
-        printf("Indtast to heltal [adskilt af mellemrum: a b]: ");
-        scanf("%d %d", &a, &b);
-
-    // Difinere sum ved at summere a og b.
+    int a, b, sum;
+    printf("Indtast to heltal [adskilt af mellemrum: a b]: ");
+    scanf("%d %d", &a, &b);
+    
     sum = a + b;
-
-        // Her udskrives summen.
-        printf("Summen er: %d\n", sum);
-
-    // Her opsummere jeg hvis summen er større en b.
+    printf("Summen er: %d\n", sum);
+    
     if (sum > b)
     {
-        // Differancen difineres, så ledes:
-        d = sum - b;
-
-            // Den opsummerende tekst udskrives.
-            printf("Summen er større end B. Differencen mellem summen og B må værre A: %d\n", d);
+        a = sum - b;  // Gem difference i a som specificeret i opgaven
+        printf("Summen er større end B. A er nu: %d\n", a);
     }
-
-    // Afslutning af main()
+    
     return 0; 
 }
